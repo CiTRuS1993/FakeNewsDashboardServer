ed76058ca1a3b49ee637b218b3a20b350a6de937
sapirgilad <sap4343@gmail.com>
2020-12-24 21:59:52 +0200

    upload tests

--- BuisnessLayer/Users/User.py
+++ BuisnessLayer/Users/User.py
@@ -1,6 +1,6 @@
 class User:
 
-    def __init__(self, role, user_id, username, password, search_history={}, tagged_tweets=[],classify_id=-1):
+    def __init__(self, role, user_id, username, password, search_history=[], tagged_tweets=[],classify_id=-1):
         self.role = role
         self.user_id = user_id
         self.username = username
@@ -10,12 +10,15 @@ class User:
         self.tagged_tweets = tagged_tweets
         self.logged_in = False;
 
-    def save_search_tweets_by_keywords(self, user_manager, search_id):
-        pass
+    def save_search_tweets_by_keywords(self, search_id):
+        self.search_history.append(search_id)
+        return True
 
     def tag_tweet(self, tweet_id):
         if tweet_id not in self.tagged_tweets:
             self.tagged_tweets.append(tweet_id)
+            return True
+        return False
 
     def get_role(self):
         return self.role
@@ -28,6 +31,7 @@ class User:
 
     def classify_tweets(self, classify_id):
         self.classify_id = classify_id
+        return True
 
     def logged_id(self):
         return self.logged_in
@@ -35,7 +39,8 @@ class User:
     def login(self, password):
         if self.password == password:
             self.logged_in = False
-        return self.logged_in
+            return True
+        return False
 
     def logout(self):
         self.logged_in = False
---
+++ tests/buisnessLayer/UserManager/testUserManager.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+from BuisnessLayer.Users.UsersManagerFacade import UserManagerFacade
+
+
+class testUserManager(TestCase):
+    def setUp(self) -> None:
+        self.UserManagerFacade = UserManagerFacade("username", "pass")
+
+    def test_register(self):
+        self.assertTrue(self.UserManagerFacade.register("sapir", "sap3232"))
+        self.assertTrue(self.UserManagerFacade.validate_user("sapir", "sap3232"))
+        self.assertFalse(self.UserManagerFacade.validate_user("yarin", "sap3232"))
+        self.assertFalse(self.UserManagerFacade.validate_user("sapir", "hadar12"))
+
+    def test_admin_exists(self):
+        self.assertFalse(self.UserManagerFacade.admin_exists("sapir", "sap3232"))
+
+    def test_save_search_tweets_by_keywords(self):
+        self.assertTrue(self.UserManagerFacade.save_search_tweets_by_keywords("sapir",1))
+
+    def test_tag_tweet(self):
+        self.assertFalse(self.UserManagerFacade.tag_tweet("sapir", 2))
+
+    def test_view_user_search_history(self):
+        self.assertFalse(self.UserManagerFacade.view_user_search_history("sa"))
+        self.assertEqual([], self.UserManagerFacade.view_user_search_history("sapir"))
+
+    def test_login(self):
+        self.assertTrue(self.UserManagerFacade.login("sapir","sap3232"))
+
+    def test_delete_user(self):
+        self.assertTrue(self.UserManagerFacade.delete_user("sapir"))
+
+    def test_is_admin(self):
+        self.assertFalse(self.UserManagerFacade.is_admin("sapir"))
+
+    def test_validate_user(self):
+        self.assertTrue(self.UserManagerFacade.validate_user("sapir", "sap3232"))
+
+    def test_classify_tweets(self):
+        self.assertFalse(self.UserManagerFacade.classify_tweets("sapir",1))
+
+    def test_user_exists(self):
+        self.assertTrue(self.UserManagerFacade.user_exists("sapir"))
