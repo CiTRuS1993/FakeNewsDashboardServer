c58ff3885378afc2c8179b57563f4bcd860ef3fe
Yarin <37544074+yarin9@users.noreply.github.com>
2020-12-24 19:52:05 +0200

    first implementation of the Service Layer

--- ServiceLayer/ExternalSystemsAPIsManagerInterface.py
+++ ServiceLayer/ExternalSystemsAPIsManagerInterface.py
@@ -2,4 +2,21 @@ class ExternalSystemsAPIsManagerInterface:
 
     def __init__(self):
         # connect Twitter
+        # timer
+        # write init func?
         pass
+
+    def searchTweetsByKeywords(self, keyword, token):
+        pass
+
+    def retrieveGoogleTrendsData(self):
+        pass
+
+    def retrieveSnopesData(self):
+        pass
+
+    # list of tokens as strings
+    def editTwittersTokens(self, tokens):
+        pass
+
+
