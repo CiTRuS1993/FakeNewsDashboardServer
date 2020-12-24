26a7a2ed187f42fc7ce6b211e153f458b519beef
CiTRuS93 <hadarradah@gmail.com>
2020-12-24 20:35:34 +0200

    thread and tests

---
+++ tests/ServiceLayer/test_DashboardFacade.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+
+class TestDashboardFacade(TestCase):
+    def test_retrieve_fake_news_data(self):
+        self.fail()
+
+    def test_google_trends_statistics(self):
+        self.fail()
+
+    def test_snopes_statistics(self):
+        self.fail()
+
+    def test_retrieve_google_trends_data(self):
+        self.fail()
+
+    def test_retrieve_snopes_data(self):
+        self.fail()
+
+    def test_config_classifier(self):
+        self.fail()
+
+    def test_search_tweets_by_keywords(self):
+        self.fail()
+
+    def test_tag_tweet(self):
+        self.fail()
+
+    def test_view_user_search_history(self):
+        self.fail()
+
+    def test_edit_twitters_tokens(self):
+        self.fail()
+
+    def test_classify_tweets(self):
+        self.fail()
+
+    def test_register(self):
+        self.fail()
+
+    def test_login(self):
+        self.fail()
+
+    def test_delete_user(self):
+        self.fail()
---
+++ tests/ServiceLayer/test_ExternalSystemsAPIsManagerInterface.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestExternalSystemsAPIsManagerInterface(TestCase):
+    def test_search_tweets_by_keywords(self):
+        self.fail()
+
+    def test_retrieve_google_trends_data(self):
+        self.fail()
+
+    def test_retrieve_snopes_data(self):
+        self.fail()
+
+    def test_edit_twitters_tokens(self):
+        self.fail()
