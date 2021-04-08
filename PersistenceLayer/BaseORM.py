b50d475ad68b4baffb3658e9d69909a84850f0d0
CiTRuS93 <hadarradah@gmail.com>
2021-04-08 19:50:20 +0300

    test and fixes

--- PersistenceLayer/BaseORM.py
+++ PersistenceLayer/BaseORM.py
@@ -2,14 +2,17 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from PersistenceLayer.database import session
 
-
+import threading
+dblock = threading.Lock()
 class BaseORM:
     def add_to_db(self):
+        dblock.acquire()
         try:
             session.add(self)
             session.commit()
         except SQLAlchemyError as e:
             session.rollback()
+        dblock.release()
 
     def update_db(self):
         session.commit()
