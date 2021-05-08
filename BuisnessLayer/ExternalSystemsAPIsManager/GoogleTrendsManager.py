2bdf2745f894e98c45c770a1b0e5d9e78aed1823
Yarin <37544074+yarin9@users.noreply.github.com>
2021-05-08 23:35:27 +0300

    fix bugs

--- BuisnessLayer/ExternalSystemsAPIsManager/GoogleTrendsManager.py
+++ BuisnessLayer/ExternalSystemsAPIsManager/GoogleTrendsManager.py
@@ -26,7 +26,8 @@ class GoogleTrendsManager:
                 flag = False
                 for trend_dict in self.all_trends[trend[0]]:
                     # print(trend_dict['date'])
-                    if trend_dict['date'] == str(date):
+                    if self.compare_dates(trend_dict['date'], date) == 0:
+                    # if trend_dict['date'] == str(date):
                         if trend_dict['id'] not in self.trends and not flag:
                             self.trends[trend_dict['id']] = {'keywords': trend[0]}
                         flag = True
@@ -47,3 +48,25 @@ class GoogleTrendsManager:
                 trends[t_id] = self.trends[t_id]
         return trends
 
+    def compare_dates(self, date1, date2):
+        if type(date1) is str:
+            if '-' in date1:
+                date1 = datetime(int(date1[:4]), int(date1[5:7]), int(date1[8:])).date()
+            else:
+                date1 = datetime(int("20"+date1[6:]), int(date1[3:5]), int(date1[:2])).date()
+        if date1.year != date2.year:
+            if date1.year > date2.year:
+                return 1
+            else:
+                return -1
+        elif date1.month != date2.month:
+            if date1.month > date2.month:
+                return 1
+            else:
+                return -1
+        elif date1.day == date2.day:
+            return 0
+        elif date1.day > date2.day:
+            return 1
+        else:
+            return -1
