dc42a6b3bbc34932237a803d5dab3af62146c456
Yarin <37544074+yarin9@users.noreply.github.com>
2021-06-12 16:22:55 +0300

    acceptance tests

--- tests/BlackBoxTests/initializationTest.py
+++ tests/BlackBoxTests/initializationTest.py
@@ -14,41 +14,18 @@ class MyTestCase(unittest.TestCase):
         dashboard = DashboardFacade(self.admin_name, self.admin_pass)
         self.assertEqual(True, len(dashboard.analysisManager.getGoogleTrendsStatistics()) > 0)
         self.assertEqual(True, dashboard.usersManager.is_admin(self.admin_name))
+        self.assertEqual(True, dashboard.externalSystemsManager.extrenalManagerLogic.twitterManager.is_connected())
 
     def test_fail(self):
         # wrong username
-        dashboard = DashboardFacade(self.admin_name + 'lalala', self.admin_pass)
-        self.assertEqual(False, len(dashboard.analysisManager.getGoogleTrendsStatistics()) > 0)
-        self.assertEqual(False, dashboard.usersManager.is_admin(self.admin_name + 'lalala'))
+        with self.assertRaises(SystemExit) as cm:
+            DashboardFacade(self.admin_name + 'lalala', self.admin_pass)
+            self.assertEqual(cm.exception, "Wrong username or password!")
+        # wrong password
+        with self.assertRaises(SystemExit) as cm:
+            DashboardFacade(self.admin_name, self.admin_pass+ 'lalala')
+            self.assertEqual(cm.exception, "Wrong username or password!")
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-    # @logger
-    def test_success(self):
-        # all valid details
-        res = self.appoint_additional_manager(self.__appointee_name, self._store_name, [])
-        self.assertTrue(res)
-
-
-    # @logger
-    def test_fail(self):
-        # store doesn't exist
-        res = self.appoint_additional_manager(self.__appointee_name, "someStoreName", [])
-        self.assertFalse(res['response'])
-        # user doesn't exist
-        res = self.appoint_additional_manager("someUser", self._store_name, [])
-        self.assertFalse(res['response'])
-        # user isn't registered
-        self.delete_user(self.__appointee_name)
-        res = self.appoint_additional_manager(self.__appointee_name, self._store_name, [])
-        self.assertFalse(res['response'])
-
-
-    # @logger
-    def tearDown(self) -> None:
-        self.remove_store(self._store_name)
-        self.delete_user(self._username)
-        self.delete_user(self.__appointee_name)
